c3b091016bca27056375f45cdd0b6a7b03f38b7e
abebdm <abebdm@uchicago.edu>
2022-07-27 10:56:29 -0500

    Added new keyboards

--- smarttvleakage/keyboard_utils/graph_to_distance.py
+++ smarttvleakage/keyboard_utils/graph_to_distance.py
@@ -13,37 +13,61 @@ args = parser.parse_args()
 f = open(args.f)
 kb = json.load(f)
 f.close()
-
-keys=sorted(kb.keys())
-print(keys)
-size=len(keys)
-
-#Create adjacency matrix
-
-M = [ [0]*size for i in range(size) ]
-
-for a,b in [(keys.index(a), keys.index(b)) for a, row in kb.items() for b in row]:
-     M[a][b] = 2 if (a==b) else 1
+#print(kb)
 
 
-for c in range(len(M)):
-	for r in range(len(M[c])):
-		if M[c][r] == 0:
-			if r!=c:
-				M[c][r] = float(M[c][r])
-				M[c][r] = np.inf
 
-#make into numpy array
-a = np.asarray(M)
-graph = csr_matrix(M)
+def combine_dicts(dict1, dict2):
+	for keys in dict2.keys():
+		for i in dict2[keys]:
+			dict1[keys].append(i)
+	return dict1
 
-#run floyd warshall
-dist_matrix, predecessors = floyd_warshall(csgraph=graph, directed=False, return_predecessors=True)
-dist_matrix = dist_matrix.tolist()
-dist_matrix.insert(0, keys)
+possibilities = []
+base_adjacency = kb['adjacency_list']
+#print(base_adjacency)
+shortcuts = kb['shortcuts']
+wraparound = kb['wraparound']
+possibilities.append(('normal',base_adjacency))
+possibilities.append(('shortcuts',combine_dicts(base_adjacency, shortcuts)))
+possibilities.append(('wraparound',combine_dicts(base_adjacency, wraparound)))
+possibilities.append(('both',combine_dicts(combine_dicts(base_adjacency, shortcuts),wraparound)))
+output = []
 
-#save
-f = open(args.o, 'w+')
-csvWriter = csv.writer(f,delimiter=',')
-csvWriter.writerows(dist_matrix)
-f.close()
+for (type,keyboard) in possibilities:
+	keys=sorted(keyboard.keys())
+	size=len(keys)
+
+	#Create adjacency matrix
+
+	M = [ [0]*size for i in range(size) ]
+
+	for a,b in [(keys.index(a), keys.index(b)) for a, row in keyboard.items() for b in row]:
+	     M[a][b] = 2 if (a==b) else 1
+
+
+	for c in range(len(M)):
+		for r in range(len(M[c])):
+			if M[c][r] == 0:
+				if r!=c:
+					M[c][r] = float(M[c][r])
+					M[c][r] = np.inf
+
+	#make into numpy array
+	a = np.asarray(M)
+	graph = csr_matrix(M)
+
+	#run floyd warshall
+	dist_matrix, predecessors = floyd_warshall(csgraph=graph, directed=False, return_predecessors=True)
+	dist_matrix = dist_matrix.tolist()
+	dist_matrix.insert(0, keys)
+
+	output.append((type,dist_matrix))
+
+for (name,matrix) in output:
+	#save
+	filename = '_'+name+'.csv'
+	f = open(args.o.replace('.csv',filename), 'w+')
+	csvWriter = csv.writer(f,delimiter=',')
+	csvWriter.writerows(matrix)
+	f.close()
