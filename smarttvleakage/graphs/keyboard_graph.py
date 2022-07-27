513e2b59f626408064b8fd590aed868e60f07130
Tejas Kannan <tkannan@geranium.a.cs.uchicago.edu>
2022-07-27 17:31:24 -0500

    Update graph parsing

--- smarttvleakage/graphs/keyboard_graph.py
+++ smarttvleakage/graphs/keyboard_graph.py
@@ -5,7 +5,7 @@ from typing import Dict, DefaultDict, List, Set
 import csv
 
 from smarttvleakage.dictionary.dictionaries import SPACE, CHANGE, BACKSPACE
-from smarttvleakage.utils.file_utils import read_json
+from smarttvleakage.utils.file_utils import read_json, read_json_gz
 
 
 class KeyboardMode(Enum):
@@ -23,17 +23,15 @@ FILTERED_KEYS = [BACKSPACE]
 
 
 def parse_graph_distances(path: str) -> Dict[str, DefaultDict[int, Set[str]]]:
-    with open(path, 'r') as fin:
-        distance_matrix = list(csv.reader(fin))
+    distance_matrix = read_json_gz(path)
 
     result: Dict[str, DefaultDict[int, Set[str]]] = dict()
 
-    for start_idx, start_key in enumerate(distance_matrix[0]):
+    for start_key, neighbors_dict in distance_matrix.items():
         result[start_key] = defaultdict(set)
 
-        for end_idx, end_key in enumerate(distance_matrix[0]):
-            distance = int(float(distance_matrix[start_idx + 1][end_idx]))
-            result[start_key][distance].add(end_key)
+        for neighbor, distance in neighbors_dict.items():
+            result[start_key][distance].add(neighbor)
 
     return result
 
@@ -42,7 +40,7 @@ class MultiKeyboardGraph:
 
     def __init__(self):
         dir_name = os.path.dirname(__file__)
-        standard_path = os.path.join(dir_name, 'samsung', 'samsung_keyboard.csv')
+        standard_path = os.path.join(dir_name, 'samsung', 'samsung_keyboard.json.gz')
         #special_one_path = os.path.join(dir_name, 'samsung', 'samsung_keyboard_special_1.csv')
 
         self._keyboards = {
@@ -63,24 +61,22 @@ class MultiKeyboardGraph:
 class SingleKeyboardGraph:
 
     def __init__(self, path: str, start_key: str):
+        # Set the start key
         self._start_key = start_key
 
-        # Get the graph without wraparound
-        with open(path, 'r') as fin:
-            self._no_wraparound = list(csv.reader(fin))
+        # Read in the precomputed shortest paths maps
+        self._no_wraparound_distances: Dict[str, DefaultDict[int, Set[str]]] = parse_graph_distances(path=path.replace('.json.gz', '_normal.json.gz'))
+        self._no_wraparound_distances_shortcuts: Dict[str, DefaultDict[int, Set[str]]] = parse_graph_distances(path=path.replace('.json.gz', '_shortcuts.json.gz'))
 
-        # Get the graph with wraparound
-        with open(path.replace('.csv', '_wraparound.csv'), 'r') as fin:
-            self._wraparound = list(csv.reader(fin))
+        wraparound_path = path.replace('.json.gz', '_wraparound.json.gz')
+        self._wraparound_distances: Dict[str, DefaultDict[int, Set[str]]] = parse_graph_distances(path=wraparound_path)
+        if os.path.exists(wraparound_path):
+            self._wraparound_distances = parse_graph_distances(path=wraparound_path)
 
-        # Read in the precomputed shortest paths map
-        start_idx = self._no_wraparound[0].index(start_key)
-
-        self._no_wraparound_distances: Dict[str, DefaultDict[int, Set[str]]] = parse_graph_distances(path=path)
-        self._wraparound_distances: Dict[str, DefaultDict[int, Set[str]]] = parse_graph_distances(path=path.replace('.csv', '_wraparound.csv'))
-
-        self._no_wraparound_distances_shortcuts: Dict[str, DefaultDict[int, Set[str]]] = parse_graph_distances(path=path.replace('.csv', '_shortcuts.csv'))
-        self._wraparound_distances_shortcuts: Dict[str, DefaultDict[int, Set[str]]] = parse_graph_distances(path=path.replace('.csv', '_shortcuts_wraparound.csv'))
+        wraparound_shortcuts_path = path.replace('.json.gz', '_all.json.gz')
+        self._wraparound_distances_shortcuts: Dict[str, DefaultDict[int, Set[str]]] = dict()
+        if os.path.exists(wraparound_shortcuts_path):
+            self._wraparound_distances_shortcuts = parse_graph_distances(path=wraparound_shortcuts_path)
 
     def get_keys_for_moves(self, num_moves: int) -> List[str]:
         return self.get_keys_for_moves_from(start_key=self._start_key, num_moves=num_moves, use_space=False)
