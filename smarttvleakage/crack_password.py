98f22960c2b7d841d5300830f2164db094eb3e22
abebdm <abebdm@uchicago.edu>
2022-08-05 13:54:18 -0500

    temp

--- smarttvleakage/crack_password.py
+++ smarttvleakage/crack_password.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-password', type=str, required=True)
 args = parser.parse_args()
 
-hashed = subprocess.run(['openssl', 'passwd', '-5', '-salt', 'agldf', args.password], capture_output=True, text=True)
+hashed = subprocess.run(['openssl', 'passwd', '-5', '-salt', 'agldf', args.password.strip()], capture_output=True, text=True)
 print(hashed.stdout)
 
 move_sequence = subprocess.run(['python3', 'keyboard_utils/word_to_move.py', '-i', args.password, '-o', 'password_moves.jsonl.gz', '-mr', '0', '-dr', '0', '-me', '0'])
@@ -21,6 +21,6 @@ with open('masks.txt', 'r') as f:
 		masks.append(line)
 
 mask_line = "--mask='"+masks[0]
-
-password = subprocess.run(['~/john-1.9.0-jumbo-1/run/john', mask_line, hashed.stdout], capture_output=True, text=True)
-print(password.stdout)
+print(mask_line)
+password = subprocess.run(['home/abebdm/john-1.9.0-jumbo-1/run/john', mask_line, hashed.stdout], capture_output=True, text=True)
+print(password.stdout)	
