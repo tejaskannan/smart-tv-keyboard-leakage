3e75a947e0506ef14daa267508e9aad0e6c23d6b
abebdm <abebdm@uchicago.edu>
2022-08-08 10:01:19 -0500

    made classifier work with fucntions

--- smarttvleakage/keyboard_utils/generate_passwords.py
+++ smarttvleakage/keyboard_utils/generate_passwords.py
@@ -3,8 +3,9 @@ import string
 import random
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--o', type=str, required=True)
-parser.add_argument('--n', type=int, required=True)
+parser.add_argument('-o', type=str, required=True)
+parser.add_argument('-n', type=int, required=True)
+parser.add_argument('-l', type=int, required=True)
 args = parser.parse_args()
 
 n=int(args.n/2)
@@ -20,14 +21,15 @@ all_chars = chars+special
 
 passwords = [[] for i in range(args.n)]
 for i in range(n):
-	length = int(random.random()*5)+5
+	# length = int(random.random()*5)+5
+	length = args.l
 	for j in range(length):
 		passwords[i].append(random.choice(chars))
 	passwords[i] = ''.join(passwords[i])
 for i in range(n):
-	length = int(random.random()*5)+4
+	length = args.l
 	passwords[i+n].append(random.choice(special))
-	for j in range(length):
+	for j in range(length-1):
 		passwords[i+n].append(random.choice(all_chars))
 	random.shuffle(passwords[i+n])
 	passwords[i+n] = ''.join(passwords[i+n])
@@ -35,3 +37,4 @@ random.shuffle(passwords)
 
 with open(args.o, 'w+') as f:
 	f.writelines('\n'.join(passwords))
+print('1')
