1a28c00ba591f06f984813c6cab5c9cee61be5db
abebdm <abebdm@uchicago.edu>
2022-08-12 14:11:26 -0500

    made word_to_move work with applletvs switch page mechanism

--- smarttvleakage/keyboard_utils/word_to_move.py
+++ smarttvleakage/keyboard_utils/word_to_move.py
@@ -9,7 +9,7 @@ from smarttvleakage.audio import Move, SAMSUNG_SELECT, SAMSUNG_KEY_SELECT, APPLE
 from smarttvleakage.utils.constants import KeyboardType
 from smarttvleakage.utils.file_utils import save_jsonl_gz
 from smarttvleakage.utils.transformations import get_keyboard_mode
-from smarttvleakage.graphs.keyboard_graph import MultiKeyboardGraph, START_KEYS, CHANGE_KEYS, SELECT_KEYS
+from smarttvleakage.graphs.keyboard_graph import MultiKeyboardGraph, START_KEYS, CHANGE_KEYS, SELECT_KEYS, SingleKeyboardGraph
 from smarttvleakage.graphs.keyboard_graph import SAMSUNG_STANDARD, APPLETV_SEARCH_ALPHABET, APPLETV_SEARCH_NUMBERS, APPLETV_SEARCH_SPECIAL, APPLETV_PASSWORD_STANDARD, APPLETV_PASSWORD_SPECIAL
 from smarttvleakage.dictionary.dictionaries import REVERSE_CHARACTER_TRANSLATION
 from datetime import datetime, timedelta
@@ -28,12 +28,30 @@ def findPath(word, shortcuts, wraparound, mr, dr, me, kb):
     for character in list(word.lower()):
         character = REVERSE_CHARACTER_TRANSLATION.get(character, character)
         distance = keyboard.get_moves_from_key(prev, character, shortcuts, wraparound, mode)
-
+        #print('link: ', keyboard.get_linked_states(prev, mode))
         if distance == -1:
-            path.append((Move(num_moves=int(keyboard.get_moves_from_key(prev, '<CHANGE>', shortcuts, wraparound, mode)), end_sound=CHANGE_KEYS[mode])))
-            prev = '<CHANGE>'
-            mode = get_keyboard_mode(prev, mode, keyboard_type=KeyboardType.SAMSUNG)
-            distance = keyboard.get_moves_from_key(prev, character, shortcuts, wraparound, mode)
+        	in_keyboard = ''
+        	found_char = False
+        	is_backwards = False
+        	for possible_keyboard in kb.get_keyboards():
+        		if found_char == True:
+        			break
+        		if character in possible_keyboard.get_characters():
+        			found_char = True
+        			in_keyboard = possible_keyboard
+        	#print(keyboard.get_keyboards())
+        	while keyboard._keyboards[mode] != in_keyboard:
+        		changer = keyboard.get_nearest_link(prev, mode, shortcuts, wraparound)
+        		# print(CHANGE_KEYS.keys())
+        		if mode in CHANGE_KEYS.keys():
+        			# print('\n')
+        			# print(CHANGE_KEYS[mode])
+        			path.append((Move(num_moves=int(keyboard.get_moves_from_key(prev, changer, shortcuts, wraparound, mode)), end_sound=CHANGE_KEYS[mode])))
+        		prev = changer
+        		linked_state = keyboard.get_linked_states(prev, mode)
+        		mode = linked_state[0][1]
+        		prev = linked_state[0][0]
+        	distance = keyboard.get_moves_from_key(prev, character, shortcuts, wraparound, mode)
 
         assert distance != -1, 'No path from {} to {}'.format(prev, character)
 
@@ -49,7 +67,7 @@ def findPath(word, shortcuts, wraparound, mr, dr, me, kb):
                 path[-1] = Move(num_moves=path[-1][0] + 2 * (x + 1), end_sound=path[-1][1])
 
         prev = character
-
+    
     return path
 
 if __name__ == '__main__':
